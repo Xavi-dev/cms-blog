0e52a8e8c0eb2fe7772060094a3a0873e942fce5
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 17:08:20 +0100

    Create acces.php

---
+++ acces.php
@@ -0,0 +1,76 @@
+<?php
+
+include("includes/header_front.php");
+
+   $dataBase = new Basemysql();
+   $db = $dataBase->connect();
+
+   $user = new User($db);
+
+   if(isset($_POST['acces'])){
+      $nick = $_POST['nick'];
+      $password = $_POST['password'];
+
+   if(empty($nick) || empty($password)){
+      $error = "Error, there are empty fields.";
+      }else{
+         if($user->acces($nick, $password)){
+            $_SESSION['blog_user_log'] = true; # Creem una sessió blog_user_log
+            $_SESSION['nick'] = $nick;
+            $message = "You are logged in.";
+         }else{
+            $error = "Error, the email or password is not correct!";
+        }
+    }
+}
+
+?>
+
+<div class="row">
+    <div class="col-sm-12">
+        <?php if (isset($error)) : ?>
+            <div class="alert bg-red-color alert-dismissible fade show message" role="alert">
+                <?php echo $error; ?>
+                <button type="button" class="btn-close" id="x-alert" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12">
+        <?php if (isset($message)) : ?>
+            <div class="alert bg-green-color alert-dismissible fade show message" role="alert">
+                <?php echo $message; ?>
+                <button type="button" class="btn-close" id="x-alert" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
+
+
+<div class="container mt-5 container-25 container-acces-50">
+
+   <form method="POST" id="userform" class="form pt-5">
+
+      <div class="input-group mb-3">
+         <input type="text" class="form-control" name="nick" placeholder="User name">
+      </div>
+
+      <div class="input-group mb-3">
+         <input type="password" class="form-control" name="password" placeholder="Password">
+      </div>
+
+      <div class="row">
+         <div class="col-sm-12 mb-1">
+            <button type="submit" name="acces" class="btn bg-green-color d-block w-100 ubuntu-r">Acces</button>
+         </div>
+      </div>
+
+   </form>
+
+</div>
+
+</div>
+
+<?php include("includes/footer_code.php") ?>
