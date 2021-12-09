80bedfc08d37061f67d5effb133906d8815c9b51
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-09 17:41:17 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php 
+
+# Rutes del projecte creades amb constants.
+
+#Ruta per admin
+define('RUTA_ADMIN', 'https://your-domain-name/cms-blog/admin/');
+
+#Ruta per front(public)
+define('RUTA_FRONT', 'https://your-domain-name/cms-blog/');
