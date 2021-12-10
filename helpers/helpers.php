7bb51cb42ac3292ea2a87494fc53364ad0663c70
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-10 17:22:36 +0100

    Update helpers.php

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,12 +1,12 @@
 <?php 
 
-// Formatejar la data sense hora //
+// Function to display date 
 
 function dateFormat($date){
     return date('d-m-Y' , strtotime($date));
 }
 
-// Formatejar la data amb hora //
+// Function to display date with time
 
 function dateFormatHour($date){
     return date('d-m-Y h:i:s' , strtotime($date));
