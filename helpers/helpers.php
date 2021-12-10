658b0d2ac9d2d23d73421945511b862bdc68abbe
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-10 09:20:22 +0100

    Update helpers.php

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -12,20 +12,5 @@ function dateFormatHour($date){
     return date('d-m-Y h:i:s' , strtotime($date));
 }
 
-function cutText($text, $limit=250){
-    $text = trim($text);
-    $text = strip_tags($text);
-    $size = strlen($text);
-    $result = '';
-    if($size <= $limit){
-        return $text;
-    }else{
-        $text = substr($text, 0, $limit);
-        $words = explode(' ', $text);
-        $result = implode(' ', $words);
-        $result .= '...';
-    }
-    return $result;
-}
 ?>
 
