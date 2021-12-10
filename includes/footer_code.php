5eeade14bd0adf08bdbdb5b22486ba5a02047c4e
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-10 17:14:50 +0100

    Update footer_code.php

--- includes/footer_code.php
+++ includes/footer_code.php
@@ -1,12 +1,12 @@
 </script>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!--     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
     <script src="../bootstrap/bootstrap.min.js"></script>
     <script src="../bootstrap/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.ckeditor.com/4.15.1/standard/ckeditor.js"></script>
+    <script src="https://cdn.ckeditor.com/4.15.1/standard/ckeditor.js"></script>  <!-- Ckeditor link to generate text editors -->
     
     <script>
-            CKEDITOR.replace('text');
+            CKEDITOR.replace('text');   <!-- Function to convert textarea to text editor ckeditor with name="text". -->
     </script>
    
   </body>
