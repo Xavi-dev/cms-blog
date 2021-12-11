dc94168c019d5d54d163db9c235ec24ba4fc6d89
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-11 13:42:13 +0100

    Update footer_code.php

--- includes/footer_code.php
+++ includes/footer_code.php
@@ -1,8 +1,8 @@
 </script>
 
 <!--     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
-    <script src="../bootstrap/bootstrap.min.js"></script>
-    <script src="../bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="bootstrap/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.ckeditor.com/4.15.1/standard/ckeditor.js"></script>  <!-- Ckeditor link to generate text editors -->
     
     <script>
